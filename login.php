32aa6e5dcc7e12c682da6700a0d6359931987f0d
Nazukida <hdz180755@gmail.com>
2025-05-13 11:03:17 +0800

    ver2.0

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'connectDataBaseLocalhost.php';
-// include 'connectDataBase.php';//connect to database
+
 $Username = $_POST['loginUsername'];
 $loginPSW = $_POST['loginPassword'];
 $sql = "SELECT * FROM touristmem WHERE username = '$Username' AND password = '$loginPSW'";
@@ -10,9 +10,17 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     $_SESSION['user'] = $Username;
     $_SESSION['pwd'] = $loginPSW;
-    echo '<script>alert("Login successfully");window.location.href="hometowns.html";</script>';
+    // 创建一个过渡页面来显示登录成功信息
+    echo '<html><body>';
+    echo '<p>Login successfully</p>';
+    echo '<a href="hometowns.html">Go to hometowns page</a>';
+    echo '</body></html>';
 } else {
-    echo '<script>alert("Username or password is incorrect");window.location.href="loginRegForm.html";</script>';
+    // 创建一个过渡页面来显示用户名或密码错误信息
+    echo '<html><body>';
+    echo '<p>Username or password is incorrect</p>';
+    echo '<a href="loginRegForm.html">Go to login page</a>';
+    echo '</body></html>';
 }
-$conn->close(); // close the session
+$conn->close();
 ?>
