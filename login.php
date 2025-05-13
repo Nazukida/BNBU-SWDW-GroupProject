eeb2bad0c7c3921bba404c4e10b2712248112484
Nazukida <hdz180755@gmail.com>
2025-05-13 11:07:19 +0800

    ver3 for php

--- login.php
+++ login.php
@@ -10,13 +10,11 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     $_SESSION['user'] = $Username;
     $_SESSION['pwd'] = $loginPSW;
-    // 创建一个过渡页面来显示登录成功信息
     echo '<html><body>';
     echo '<p>Login successfully</p>';
     echo '<a href="hometowns.html">Go to hometowns page</a>';
     echo '</body></html>';
 } else {
-    // 创建一个过渡页面来显示用户名或密码错误信息
     echo '<html><body>';
     echo '<p>Username or password is incorrect</p>';
     echo '<a href="loginRegForm.html">Go to login page</a>';
--- register.php
+++ register.php
@@ -14,27 +14,23 @@ if ($RePwd === $conRePwd) {
     if ($checkResult->num_rows === 0) { //new user
         $sqlnew = "INSERT INTO touristmem (username, password) VALUES ('$Username', '$RePwd')";
         if ($conn->query($sqlnew) === TRUE) {
-            // 创建一个过渡页面来显示注册成功信息
             echo '<html><body>';
             echo '<p>register successfully, please login</p>';
             echo '<a href="loginRegForm.html">Go to login page</a>';
             echo '</body></html>';
         } else {
-            // 创建一个过渡页面来显示注册失败信息
             echo '<html><body>';
             echo '<p>register failed: '. $conn->error.'</p>';
             echo '<a href="loginRegForm.html">Go to registration page</a>';
             echo '</body></html>';
         }
     } else {
-        // 创建一个过渡页面来显示用户名已存在信息
         echo '<html><body>';
         echo '<p>username already exists</p>';
         echo '<a href="loginRegForm.html">Go to registration page</a>';
         echo '</body></html>';
     }
 } else {
-    // 创建一个过渡页面来显示两次密码不匹配信息
     echo '<html><body>';
     echo '<p>two passwords do not match</p>';
     echo '<a href="loginRegForm.html">Go to registration page</a>';
